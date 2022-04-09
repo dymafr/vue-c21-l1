43865e331fb231366f9ba1d2f8487850f9f61505
dymafr <contact@dyma.fr>
2022-04-09 14:42:00 +0200

    maj

--- src/features/boutique/Boutique.vue
+++ src/features/boutique/Boutique.vue
@@ -19,96 +19,85 @@ import type {
 import { DEFAULT_FILTERS } from './data/filters';
 import { fetchProducts } from '../../shared/services/product.service';
 import { pageKey } from '../../shared/injectionKeys/pageKey';
+import { useCart } from './stores/cartStore';
+import { useProducts } from './stores/productStore';
 
-const state = reactive<{
-  products: ProductInterface[];
-  cart: ProductCartInterface[];
-  filters: FiltersInterface;
-  page: number;
-  isLoading: boolean;
-  moreResults: boolean;
-}>({
-  products: [],
-  cart: [],
-  filters: { ...DEFAULT_FILTERS },
-  page: 1,
-  isLoading: true,
-  moreResults: true,
-});
+const productStore = useProducts();
+const cartStore = useCart();
 
-provide(pageKey, toRef(state, 'page'));
+// provide(pageKey, toRef(state, 'page'));
 
-watch([() => state.filters.priceRange, () => state.filters.category], () => {
-  state.page = 1;
-  state.products = [];
-});
+// watch([() => state.filters.priceRange, () => state.filters.category], () => {
+//   state.page = 1;
+//   state.products = [];
+// });
 
-watchEffect(async () => {
-  state.isLoading = true;
-  const products = await fetchProducts(state.filters, state.page);
-  if (Array.isArray(products)) {
-    state.products = [...state.products, ...products];
-    if (products.length < 20) {
-      state.moreResults = false;
-    }
-  } else {
-    state.products = [...state.products, products];
-  }
-  state.isLoading = false;
-});
+// watchEffect(async () => {
+//   state.isLoading = true;
+//   const products = await fetchProducts(state.filters, state.page);
+//   if (Array.isArray(products)) {
+//     state.products = [...state.products, ...products];
+//     if (products.length < 20) {
+//       state.moreResults = false;
+//     }
+//   } else {
+//     state.products = [...state.products, products];
+//   }
+//   state.isLoading = false;
+// });
 
-function addProductToCart(productId: string): void {
-  const product = state.products.find((product) => product._id === productId);
-  if (product) {
-    const productInCart = state.cart.find(
-      (product) => product._id === productId
-    );
-    if (productInCart) {
-      productInCart.quantity++;
-    } else {
-      state.cart.push({ ...product, quantity: 1 });
-    }
-  }
-}
+// function addProductToCart(productId: string): void {
+//   const product = state.products.find((product) => product._id === productId);
+//   if (product) {
+//     const productInCart = state.cart.find(
+//       (product) => product._id === productId
+//     );
+//     if (productInCart) {
+//       productInCart.quantity++;
+//     } else {
+//       state.cart.push({ ...product, quantity: 1 });
+//     }
+//   }
+// }
 
-function removeProductFromCart(productId: string): void {
-  const productFromCart = state.cart.find(
-    (product) => product._id === productId
-  );
-  if (productFromCart?.quantity === 1) {
-    state.cart = state.cart.filter((product) => product._id !== productId);
-  } else {
-    productFromCart.quantity--;
-  }
-}
+// function removeProductFromCart(productId: string): void {
+//   const productFromCart = state.cart.find(
+//     (product) => product._id === productId
+//   );
+//   if (productFromCart?.quantity === 1) {
+//     state.cart = state.cart.filter((product) => product._id !== productId);
+//   } else {
+//     productFromCart.quantity--;
+//   }
+// }
 
-function updateFilter(filterUpdate: FilterUpdate) {
-  if (filterUpdate.search !== undefined) {
-    state.filters.search = filterUpdate.search;
-  } else if (filterUpdate.priceRange) {
-    state.filters.priceRange = filterUpdate.priceRange;
-  } else if (filterUpdate.category) {
-    state.filters.category = filterUpdate.category;
-  } else {
-    state.filters = { ...DEFAULT_FILTERS };
-  }
-}
+// function updateFilter(filterUpdate: FilterUpdate) {
+//   if (filterUpdate.search !== undefined) {
+//     state.filters.search = filterUpdate.search;
+//   } else if (filterUpdate.priceRange) {
+//     state.filters.priceRange = filterUpdate.priceRange;
+//   } else if (filterUpdate.category) {
+//     state.filters.category = filterUpdate.category;
+//   } else {
+//     state.filters = { ...DEFAULT_FILTERS };
+//   }
+// }
 
-const cartEmpty = computed(() => state.cart.length === 0);
+// const cartEmpty = computed(() => state.cart.length === 0);
 
-const filteredProducts = computed(() => {
-  return state.products.filter((product) => {
-    if (
-      product.title
-        .toLocaleLowerCase()
-        .startsWith(state.filters.search.toLocaleLowerCase())
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-});
+// const filteredProducts = computed(() => {
+//   return state.products.filter((product) => {
+//     if (
+//       product.title
+//         .toLocaleLowerCase()
+//         .startsWith(state.filters.search.toLocaleLowerCase())
+//     ) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+// });
 </script>
 
 <template>
